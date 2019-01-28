c2d3e9ba5f05d90867b195964ccebd2f0b8ec869
Orlando <orlandosanch3z@gmail.com>
2019-01-27 21:39:44 -0800

    now with sqlite

---
+++ formulageneral/DB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace formulageneral
+{
+    //ironicamnete cree esta clase para no repetir tanto codigo, pero no me di cuenta que tenia que guardar los cambios en dbrowser para que se guardara mis tablas y ya no quise usar esto o guardarlo
+    //pero si funciona esta clase
+    class DB
+    {
+        public SQLiteConnection myconnect;
+       public DB()
+        {
+            myconnect = new SQLiteConnection("Data Source= registros.sqlite3");
+
+        }
+        public void create()
+        {
+            if (!File.Exists("./registros.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("registros.sqlite3");
+
+            }
+        }
+        public void open()
+        {
+            if(myconnect.State != System.Data.ConnectionState.Open)
+            {
+                myconnect.Open();
+            }
+        }
+        public void close()
+        {
+            if(myconnect.State != System.Data.ConnectionState.Closed)
+            {
+                myconnect.Clone();
+            }
+        }
+     
+
+    }
+}
--- formulageneral/Form1.cs
+++ formulageneral/Form1.cs
@@ -7,40 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+
+using System.Data.SQLite;
+using System.IO;
+
 namespace formulageneral
 {
     public partial class Form1 : Form
     {
+        private SQLiteConnection con;
+        private SQLiteCommand cmd;
+        private SQLiteDataAdapter db;
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        
+        private void setcon()
+        {
+            con = new SQLiteConnection("Data Source=registros.sqlite3;Version=3;New=False;Compress=true;");
+        }
+        private void Exe(string query)
+        {
+            setcon();
+            con.Open();
+            cmd = con.CreateCommand();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            con.Close();
 
-        //   SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Reborn 3.0\Documents\GitHub\formulageneral\formulageneral\datos.mdf;Integrated Security = True");
-        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\datos.mdf;Integrated Security=True");
+        }
+        private void loaddata()
+        {
+            setcon();
+            con.Open();
+            cmd = con.CreateCommand();
+            string cmdt = "select * from datos";
+            db = new SQLiteDataAdapter(cmdt, con);
+            ds.Reset();
+            db.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+       
         private void btnresolver_Click(object sender, EventArgs e)
         {
             try
             {
-                con.Open();
-                String st = "insert into datos (fecha,A,B,C,x1,x2)values(@fecha,@A,@B,@C,@x1,@x2)";
-                SqlCommand cmd = new SqlCommand(st,con);
-
+                
+              
                 double a, b, c, x1, x2;
-
+                int id=0;
                 a = Convert.ToDouble(txta.Text);
                 b = Convert.ToDouble(txtb.Text);
                 c = Convert.ToDouble(txtc.Text);
-                cmd.Parameters.AddWithValue("@fecha", DateTime.Now.ToString("yyyy-MM-dd HH:mm"));
-                cmd.Parameters.AddWithValue("@A", txta.Text);
-                cmd.Parameters.AddWithValue("@B", txtb.Text);
-                cmd.Parameters.AddWithValue("@C", txtc.Text);
-             
+
+               
+
+
                 x1 = (-b + (Math.Sqrt((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
                 x2 = (-b + (Math.Sqrt((Math.Pow(b, 2)) - (4 * a * c)))) / (2 * a);
                 lblresultado.Text = "X1= " + x1.ToString();
                 lblx2.Text = "X2 = " + x2.ToString();
-                cmd.Parameters.AddWithValue("@x1", x1.ToString());
-                cmd.Parameters.AddWithValue("@x2", x2.ToString());
-                cmd.ExecuteNonQuery();
-                con.Close();
+
+                /*
+                string query = @"INSERT INTO datos ( 'id' , 'fecha' , 'A' , 'B' , 'C' , 'x1' , 'x2' ) VALUES (@id,@fecha,@A,@B,@C,@x1,@x2)";
+            
+               
+                cmd.Parameters.AddWithValue("@fecha", );
+                cmd.Parameters.AddWithValue("@A", txta.Text);
+                cmd.Parameters.AddWithValue("@B", txtb.Text);
+                cmd.Parameters.AddWithValue("@C", txtc.Text);
+          
+                cmd.Parameters.AddWithValue("@x1", lblresultado.Text);
+                cmd.Parameters.AddWithValue("@x2", lblx2.Text);
+                */
+              
+                string query = string.Format("insert into datos values ('{0}','{1}','{2}','{3}','{4}','{5}')", DateTime.Now.ToString("yyyy-MM-dd HH:mm"), txta.Text, txtb.Text, txtc.Text, lblresultado.Text, lblx2.Text);
+                Exe(query);
+            
+                
+
             }
             catch(Exception ex)
             {
@@ -51,13 +97,11 @@ namespace formulageneral
         public Form1()
         {
             InitializeComponent();
-            con.Open();
-            SqlDataAdapter sqldata = new SqlDataAdapter("select * from datos", con);
-            DataTable dtlb = new DataTable();
-            sqldata.Fill(dtlb);
-            dataGridView1.DataSource = dtlb;
-            con.Close();
-        }
+            loaddata();
+            
+
+       
+    }
 
         private void lblresultado_Click(object sender, EventArgs e)
         {
